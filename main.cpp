2a03953d1d6f2110223c2cd21da2142069c63afe
MrZorg111 <yodji333@mail.ru>
2022-02-27 00:18:54 +0300

    Start Games sec!

--- main.cpp
+++ main.cpp
@@ -1,23 +1,5 @@
 #include <iostream>
 
-/*
-
- * Вначале игроки по очереди расставляют корабли, начиная с самых маленьких и заканчивая большими.
- * Маленькие корабли в одну клетку расставляются с помощью указания лишь одной клетки-точки на поле, к примеру 2,1.
- * Корабли размерностью от двух клеток и выше, расставляются с помощью координат их начала и конца, к примеру: 0,1 - 0,3.
- * Корабли могут быть установлены только строго вертикально, или горизонтально. Установка корабля по диагонали — недопустимо,
- * в случае которого об этом тоже надо сообщить пользователю. Если корабль не может быть размещён на заданных клетках из-за того,
- * что он столкнётся с другим кораблём (окажется на его клетках) или выйдет за пределы поля вообще,
- * то игроку должно быть сделано предупреждение, после которого он должен переставить корабль на новые валидные координаты.
- * Замечу, в нашем упрощённом варианте игры, мы не будем обсчитывать соприкосновения кораблей друг с другом,
- * а потому корабли вполне можно будет размещать друг к другу вплотную — борт к борту.
- * После того как все корабли были расставлены начинается игра. Игроки по очереди атакуют друг друга, называя координаты выстрела.
- * После выстрела, в консоль выводится информация о его результате: попал или мимо.
- * Если выстрел успешен, клетка, на которой был корабль (или его фрагмент) затирается и становится пустой.
- * Игра заканчивается тогда, когда все корабли одной из сторон будут полностью уничтожены.
- * Как только это произойдёт в консоль выводится информация с номером игрока, который победил.
- * */
-
 char field(int b, int c) {
     char a [13][13] {{' ', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '},
                      {' ', '+', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '+'},
@@ -177,46 +159,98 @@ int main() {
     }
     player_2 = false;
     for (int g = 0; ; g++) {
-        if (player_2){
-            std::cout << "Player number two, you start first. Enter the firing coordinates:\n";
-        }else {
-            std::cout << "Player number one, you start first. Enter the firing coordinates:\n";
-        }
-        for (int f = 0; f < 13; f++) {
+        do {
+            good = true;
             if (player_2) {
-                for (int a = 0; a < 13; a++) {
-                    std::cout << filed_player_2[f][a] << " ";
-                }
-                std::cout << "\t\t\t";
-                for (int a_2 = 0; a_2 < 13; a_2++) {
-                    std::cout << field_fire_p2[f][a_2] << " ";
-                }
+                std::cout << "Player number two. Enter the firing coordinates:\n";
+            } else {
+                std::cout << "Player number one, you start first. Enter the firing coordinates:\n";
             }
-            else {
-                for (int a = 0; a < 13; a++) {
-                    std::cout << filed_player_1[f][a] << " ";
+            for (int f = 0; f < 13; f++) {
+                if (player_2) {
+                    for (int a = 0; a < 13; a++) {
+                        std::cout << filed_player_2[f][a] << " ";
+                    }
+                    std::cout << "\t\t\t";
+                    for (int a_2 = 0; a_2 < 13; a_2++) {
+                        std::cout << field_fire_p2[f][a_2] << " ";
+                    }
+                } else {
+                    for (int a = 0; a < 13; a++) {
+                        std::cout << filed_player_1[f][a] << " ";
+                    }
+                    std::cout << "\t\t\t";
+                    for (int a_2 = 0; a_2 < 13; a_2++) {
+                        std::cout << field_fire_p1[f][a_2] << " ";
+                    }
                 }
-                std::cout << "\t\t\t";
-                for (int a_2 = 0; a_2 < 13; a_2++) {
-                    std::cout << field_fire_p1[f][a_2] << " ";
-                }
+                std::cout << std::endl;
             }
-            std::cout << std::endl;
-        }
-    std::cout << "Enter the x coordinate:\n";
-    std::cin >> x;
-    std::cout << "Enter the y coordinate:\n";
-    std::cin >> y;
+            std::cout << "Enter the x coordinate:\n";
+            std::cin >> x;
+            std::cout << "Enter the y coordinate:\n";
+            std::cin >> y;
+            if (field_fire_p1[y + 2][x + 2] || field_fire_p2[y + 2][x + 2] == 'X') {
+                std::cout << "You've already shot there!\n";
+                good = false;
+            }
+        }while ((x < 0 || y < 0 || x > 9 || y > 9) && !good);
 
     if (player_2) {
         field_fire_p2[y + 2][x + 2] = 'X';
     }else {
         field_fire_p1[y + 2][x + 2] = 'X';
     }
+    if (player_2) {
+        if (filed_player_1[y + 2][x + 2] == 'O') {
+            filed_player_1[y + 2][x + 2] = 'X';
+            std::cout << "Got!!!\n";
+            if (filed_player_1[y + 1][x + 1] != 'O' &&  filed_player_1[y + 1][x + 2] != 'O' &&  filed_player_1[y + 1][x + 3] != 'O' &&
+                    filed_player_1[y + 2][x + 1] != 'O' &&  filed_player_1[y + 2][x + 3] != 'O' && filed_player_1[y + 3][x + 1] != 'O' &&
+                    filed_player_1[y + 3][x + 2] != 'O' && filed_player_1[y + 3][x + 3] != 'O') {
+                std::cout << "Killed!!!\n";
+            }
+        }else {
+            filed_player_1[y + 2][x + 2] = 'X';
+        }
+    }
+    else if (filed_player_2[y + 2][x + 2] == 'O') {
+            filed_player_2[y + 2][x + 2] = 'X';
+            std::cout << "Got!!!\n";
+            if (filed_player_2[y + 1][x + 1] && filed_player_2[y + 1][x + 2] && filed_player_2[y + 1][x + 3] &&
+                filed_player_2[y + 2][x + 1] && filed_player_2[y + 2][x + 3] && filed_player_2[y + 3][x + 1] &&
+                filed_player_2[y + 3][x + 2] && filed_player_2[y + 3][x + 3] != 'O') {
+                std::cout << "Killed!!!\n";
+            }
+        }else {
+            filed_player_2[y + 2][x + 2] = 'X';
+        }
+       int count = 0;
+        for (int vic = 0; vic < 13; vic ++) {
+            for (int tory = 0; tory < 13; tory++) {
+                if (player_2) {
+                    if (filed_player_1[tory][vic] == 'O') {
+                        count++;
+                    }
+                }else {
+                    if (filed_player_2[tory][vic] == 'O') {
+                        count++;
+                    }
+                }
+            }
+        }
+        if (player_2 && count == 0) {
+            std::cout << "Player two WIN!!!";
+            return 0;
+        }else if (count == 0) {
+            std::cout << "Player one WIN!!!";
+            return 0;
+        }
 
-
-
-
-
+    if (player_2) {
+        player_2 = false;
+    }else {
+        player_2 = true;
+    }
     }
 }
